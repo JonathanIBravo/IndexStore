cf2b780137fda9bc8edc33c377de12c59808d1b5
Jonathan Bravo <j.ivanbravo98@gmail.com>
2023-01-09 22:43:12 -0300

    utilizando array

--- js/main.js
+++ js/main.js
@@ -1,50 +1,108 @@
 alert("¡Bienvenidos a IndexStore! Nuestra tienda online de zapatillas importadas!")
-alert("¡GANO LA SELECCION! ¡¡Por eso hoy tenemos descuentos y beneficios para vos!!")
-alert("A continuacion te mostraremos una lista con el stock con el cual contamos, para salir Ingrese 0.")
+alert("¡Primer mes del año y para todo hay una excusa! ¡¡Por eso hoy tenemos descuentos y beneficios para vos!!")
+alert("A continuacion te mostraremos una lista con el stock con el cual contamos.")
 
-let selecionarProductos = Number(prompt("1-Air force one $1400 2-Jordan 1 $1200 3-Goretex $1000"))
-let seleccionarCantidad; 
-let total = 0;
+// let selecionarProductos = Number(prompt("1-Air force one $1400 2-Jordan 1 $1200 3-Goretex $1000"))
+// let seleccionarCantidad; 
+// let total = 0;
 
-const cantidad =(cant, precio) => {
-    return cant * precio
-}
+// const cantidad =(cant, precio) => {
+//     return cant * precio
+// }
 
-while (selecionarProductos !=0){
-    switch(selecionarProductos){
-        case 1:
-            seleccionarCantidad = Number(prompt("El producto seleccionado son las Air force one, indique cantidad que desea comprar."))
-            total += cantidad (seleccionarCantidad, 1400)
-            break;
+// while (selecionarProductos !=0){
+//     switch(selecionarProductos){
+//         case 1:
+//             seleccionarCantidad = Number(prompt("El producto seleccionado son las Air force one, indique cantidad que desea comprar."))
+//             total += cantidad (seleccionarCantidad, 1400)
+//             break;
 
-        case 2:
-            seleccionarCantidad = Number(prompt("El producto seleccionado son las Jordan 1, indique cantidad que desea comprar."))
-            total += cantidad (seleccionarCantidad, 1200)
-            break;
+//         case 2:
+//             seleccionarCantidad = Number(prompt("El producto seleccionado son las Jordan 1, indique cantidad que desea comprar."))
+//             total += cantidad (seleccionarCantidad, 1200)
+//             break;
 
-        case 3:
-            seleccionarCantidad = Number(prompt("El producto seleccionado son las Goretex, indique cantidad que desea comprar."))
-            total += cantidad (seleccionarCantidad, 1000)
-            break;
+//         case 3:
+//             seleccionarCantidad = Number(prompt("El producto seleccionado son las Goretex, indique cantidad que desea comprar."))
+//             total += cantidad (seleccionarCantidad, 1000)
+//             break;
 
-        default:
-            break;
+//         default:
+//             break;
+//     }
+//     // alert("¡Sigue comprando y obtén beneficios en tu envios!")
+//     selecionarProductos = Number(prompt("1-Air force one $1400 2-Jordan 1 $1200 3-Goretex $1000, si desea finalizar ¡Ingrese 0!"))
+// }
+
+
+// alert("Superando los $1500, te brindamos ¡ENVIOS GRATIS A TODO EL PAIS!")
+
+// alert("El total de la compra es de: $" + total)
+
+// const envio = () => {
+//     if (total >= 1500){
+//         alert("Superaste de $1500, tenes ¡ENVIOS GRATIS A TODO EL PAIS! ")
+//     } else {
+//         total += 500
+//         alert("El costo de envio es de $500, el total es: $"  + total)
+//     }
+// }
+// envio()
+
+
+const carrito = []
+
+const ordenarMenorMayor = () => {
+    productos.sort((a, b) => a.precio - b.precio)
+    mostrarListaOrdenada()
+};
+
+const ordenarMayorMenor = () => {
+    productos.sort((a, b) => b.precio - a.precio)
+    mostrarListaOrdenada()
+};
+
+const mostrarListaOrdenada = () => {
+    const listaDeProductos = productos.map(producto => {
+        return '- ' + producto.nombre+ ' $' +producto.precio
+    }) 
+    alert('Lista de precios: '+ '\n\n' +listaDeProductos.join('\n'))
+
+    comprarProductos(listaDeProductos)
+};
+
+const comprarProductos = (listaDeProductos) => {
+    let productoNombre = ''
+    let productoCantidad = 0
+    let otroProducto = false
+
+    do{
+        productoNombre = prompt('¿Que producto queres comprar?' + '\n\n' +listaDeProductos.join('\n'))
+        productoCantidad = parseInt (prompt('¿Cuantos deseas cumprar?'))
+
+        const producto = productos.find (producto => producto.nombre.toLowerCase() === productoNombre.toLowerCase())
+        
+        if (producto) {
+            agregarAlCarrito(producto, producto.id, productoCantidad)
+        } else{
+            alert ('El producto no se encuentra en el catalogo.')
+        }
+
+        otroProducto = confirm ('¿Queres agregar otro producto?')
+    }while(otroProducto)
+};
+
+const agregarAlCarrito = (producto, productoId, productoCantidad) => {
+    const productoRepetido = carrito.find (producto => producto.id === productoId)
+    if (!productoRepetido){
+        producto.cantidad += productoCantidad
+        carrito.push(producto)
+    } else{
+        productoRepetido.cantidad += productoCantidad
     }
-    // alert("¡Sigue comprando y obtén beneficios en tu envios!")
-    selecionarProductos = Number(prompt("1-Air force one $1400 2-Jordan 1 $1200 3-Goretex $1000, si desea finalizar ¡Ingrese 0!"))
-}
+};
 
 
-alert("Superando los $1500, te brindamos ¡ENVIOS GRATIS A TODO EL PAIS!")
 
-alert("El total de la compra es de: $" + total)
 
-const envio = () => {
-    if (total >= 1500){
-        alert("Superaste de $1500, tenes ¡ENVIOS GRATIS A TODO EL PAIS! ")
-    } else {
-        total += 500
-        alert("El costo de envio es de $500, el total es: $"  + total)
-    }
-}
-envio()
+ordenarMenorMayor()
---
+++ js/stock.js
@@ -0,0 +1,204 @@
+const productos = [
+    {
+        id:1,
+        nombre: "Aire Force One",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:210,
+        talle:"44",
+        cantidad: 0,
+    },
+    
+    {
+        id:2,
+        nombre: "Adidas Falcon",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:130,
+        talle: "",
+        cantidad: 0,
+    },
+
+    {
+        id:3,
+        nombre: "Nike Cortez",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:145,
+        talle: "",
+        cantidad: 0,
+    },
+
+    {
+        id:4,
+        nombre: "Nike Revolution",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:135,
+        talle: "",
+        cantidad: 0,
+    },
+
+    {
+        id:5,
+        nombre: "Nike zoom",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:116,
+        talle: "",
+        cantidad: 0,
+    },
+    
+    {
+        id:6,
+        nombre: "Adidas SuperStar ",
+        tipo: "Zapatilla",
+        descripcion: "Blancas",
+        precio:117,
+        talle: "40",
+        cantidad: 0,
+    },
+
+    {
+        id:7,
+        nombre: "Converse",
+        tipo: "Botita",
+        descripcion: "Negras con plataforma blanca",
+        precio:105,
+        talle: "42",
+        cantidad: 0,
+    },
+
+    {
+        id:8,
+        nombre: "Jordan 1",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:375,
+        talle: "41",
+        cantidad: 0,
+    },
+
+    {
+        id:9,
+        nombre: "New balance Urbanas",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:117,
+        talle: "43",
+        cantidad: 0,
+    },
+
+    {
+        id:10,
+        nombre: "Nike Dunk Hallowen",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:300,
+        talle: "40",
+        cantidad: 0,
+    },
+
+    {
+        id:11,
+        nombre: "Adidas Ultra boost",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:156,
+        talle: "41",
+        cantidad: 0,
+    },
+
+    {
+        id:12,
+        nombre: "Air max",
+        tipo: "Zapatillas",
+        descripcion: "",
+        precio:148,
+        talle: "40",
+        cantidad: 0,
+    },
+
+
+    {
+        id:13,
+        nombre: "Converse con plataforma",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:110,
+        talle: "39",
+        cantidad: 0,
+    },
+
+    {
+        id:14,
+        nombre: "Jordan  Dior",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:120,
+        talle: "42",
+        cantidad: 0,
+    },
+
+    {
+        id:15,
+        nombre: "Nike Shadow",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:70,
+        talle: "40",
+        cantidad: 0,
+    },
+
+    {
+        id:16,
+        nombre: "Vans con plataforma",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:75,
+        talle: "39",
+        cantidad: 0,
+    },
+
+    {
+        id:17,
+        nombre: "Vans Clasicas",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:70,
+        talle: "40 ",
+        cantidad: 0,
+    },
+
+    {
+        id:18,
+        nombre: "Nike 97",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:122,
+        talle: "44",
+        cantidad: 0,
+    },
+
+    {
+        id:19,
+        nombre: "Nike Presto",
+        tipo: "Zapatilla",
+        descripcion: "",
+        precio:105,
+        talle: "39",
+        cantidad: 0,
+    },
+
+    {
+        id:20,
+        nombre: "Air Force Mid",
+        tipo: "Zapatillas",
+        descripcion: "",
+        precio:138,
+        talle: "43",
+        cantidad: 0,
+    },
+
+
+]
