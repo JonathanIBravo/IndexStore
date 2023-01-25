b63cd51f767e504a153290660dbc60e99627ad9b
Jonathan Bravo <j.ivanbravo98@gmail.com>
2023-01-24 23:49:44 -0300

    json & storage

---
+++ data/stock.js
@@ -0,0 +1,205 @@
+const stockProductos = [
+    {
+        id:1,
+        nombre: "Aire Force One",       
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:210,
+        talle:"44",
+        cantidad: 1,
+        img: "../public/img/AireForceOne.jpg",
+    },
+    
+    {
+        id:2,
+        nombre: "Adidas Falcon",      
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:130,
+        talle: "",
+        cantidad: 1,
+        img: "../public/img/adidasfalcon.jpg",
+    },
+
+    {
+        id:3,
+        nombre: "Nike Cortez",       
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:145,
+        talle: "",
+        cantidad: 1,
+        img: "../public/img/nikecortez.jpg",
+    },
+
+    {
+        id:4,
+        nombre: "Nike Revolution 5",      
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:135,
+        talle: "",
+        cantidad: 1,
+        img: "../public/img/nikerevolution5.jpg"
+    },
+
+    {
+        id:5,
+        nombre: "Nike zoom",   
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:116,
+        talle: "",
+        cantidad: 1,
+        img: "../public/img/nikezoom.jpg",
+    },
+    
+    {
+        id:6,
+        nombre: "Adidas SuperStar ",
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:117,
+        talle: "40",
+        cantidad: 1,
+        img: "../public/img/adidassuperstar.jpg",
+    },
+
+    {
+        id:7,
+        nombre: "Converse Botitas",
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:105,
+        talle: "42",
+        cantidad: 1,
+        img: "../public/img/conversebotitas.jpg",
+    },
+
+    {
+        id:8,
+        nombre: "Jordan 1",       
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:375,
+        talle: "41",
+        cantidad: 1,
+        img: "../public/img/jordan1.jpg",
+    },
+
+    {
+        id:9,
+        nombre: "New balance Urbanas",        
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:117,
+        talle: "43",
+        cantidad: 1,
+        img: "../public/img/newbalanceurbana.jpg",
+    },
+
+    {
+        id:10,
+        nombre: "Nike Dunk Hallowen",        
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:300,
+        talle: "40",
+        cantidad: 1,
+        img: "../public/img/nikedunkhallowen.jpg",
+    },
+
+    {
+        id:11,
+        nombre: "Adidas Ultra boost",
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:156,
+        talle: "41",
+        cantidad: 1,
+        img: "../public/img/adidasultrabost.jpg",
+    },
+
+    {
+        id:12,
+        nombre: "Air max 90",
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:148,
+        talle: "40",
+        cantidad: 1,
+        img: '../public/img/airmax90.jpg'
+    },
+
+
+    {
+        id:13,
+        nombre: "Converse con plataforma", 
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:110,
+        talle: "39",
+        cantidad: 1,
+        img: "../public/img/200010557144001-1.jpg",
+    },
+
+    {
+        id:14,
+        nombre: "Jordan  Dior", 
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:120,
+        talle: "42",
+        cantidad: 1,
+        img: "../public/img/jordandior.jpg",
+        
+    },
+
+    {
+        id:15,
+        nombre: "Nike Shadow",      
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:70,
+        talle: "40",
+        cantidad: 1,
+        img: "../public/img/nikeshadowp.jpg",
+    },
+
+    {
+        id:16,
+        nombre: "Vans con plataforma",        
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:75,
+        talle: "39",
+        cantidad: 1,
+        img: "../public/img/vanzconplataforma.jpg",
+    },
+
+    {
+        id:17,
+        nombre: "Vans Clasicas",        
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:70,
+        talle: "40 ",
+        cantidad: 1,
+        img: "../public/img/vanzclasicas.jpg",
+    },
+
+    {
+        id:18,
+        nombre: "Nike 97",       
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:122,
+        talle: "44",
+        cantidad: 1,
+        img: "../public/img/nike97.jpg",
+    },
+
+    {
+        id:19,
+        nombre: "Nike Presto",        
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:105,
+        talle: "39",
+        cantidad: 1,
+        img: "../public/img/nikepresto.jpg",
+    },
+
+    {
+        id:20,
+        nombre: "Air Force Mid",
+        desc: "Muy bonitas para combinar con tus Outfis",
+        precio:138,
+        talle: "43",
+        cantidad: 1,
+        img: "../public/img/airforcemid.jpg",
+    },
+
+
+]
